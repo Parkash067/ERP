2b1935d66d32746c1c77d03d64d93c0dd2e80aba
parkash067 <pk_bscs67@yahoo.com>
2018-01-04 23:41:39 +0500

    add function which is responsible to create invoice lines from cron job

--- custom_contracts/contracts.py
+++ custom_contracts/contracts.py
@@ -18,6 +18,30 @@ class custom_contract(osv.osv):
         'include_cdr_amount': fields.boolean('Calculate amount from CDR files', store=True),
     }
 
+
+    # This is the function which is reponsible to create invoice lines from cron job we must modified these lines
+    def _prepare_invoice_line(self, cr, uid, line, fiscal_position, context=None):
+        fpos_obj = self.pool.get('account.fiscal.position')
+        res = line.product_id
+        account_id = res.property_account_income.id
+        if not account_id:
+            account_id = res.categ_id.property_account_income_categ.id
+        account_id = fpos_obj.map_account(cr, uid, fiscal_position, account_id)
+
+        taxes = res.taxes_id or False
+        tax_id = fpos_obj.map_tax(cr, uid, fiscal_position, taxes, context=context)
+        values = {
+            'name': line.name,
+            'account_id': account_id,
+            'account_analytic_id': line.analytic_account_id.id,
+            'price_unit': line.price_unit or 0.0,
+            'quantity': line.quantity,
+            'uos_id': line.uom_id.id or False,
+            'product_id': line.product_id.id or False,
+            'invoice_line_tax_id': [(6, 0, tax_id)],
+        }
+        return values
+
     def _prepare_invoice_lines(self, cr, uid, contract, fiscal_position_id, context=None):
         fpos_obj = self.pool.get('account.fiscal.position')
         fiscal_position = None
@@ -27,6 +51,8 @@ class custom_contract(osv.osv):
         for line in contract.recurring_invoice_line_ids:
             values = self._prepare_invoice_line(cr, uid, line, fiscal_position, context=context)
             invoice_lines.append((0, 0, values))
+        print ">>>>>>>>>>>>>>>>>>>>>>>>.invoice lines>>>>>>>>>>>>>"
+        print invoice_lines
         return invoice_lines
 
     def _prepare_invoice(self, cr, uid, contract, context=None):
