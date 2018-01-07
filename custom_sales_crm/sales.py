4b5b877cf0d0377aac5e123a2817791ba4766852
parkash067 <pk_bscs67@yahoo.com>
2018-01-07 16:45:20 +0500

    move function from sales to contract

--- custom_sales_crm/sales.py
+++ custom_sales_crm/sales.py
@@ -69,21 +69,4 @@ class cdr(osv.osv):
 
     }
 
-    def read_logs(self,file_path):
-        #make sure using r'filepath' to mean its a string literal
-        fl = open(file_path,'r')
-        end_lst = []
-        fl_all = fl.read()
-        lst_rec = fl_all.split('\n')
-        for rec in lst_rec:
-            rec_lst = rec.split(',')
-            if len(rec_lst) > 1:
-                dct = {}
-                for ind, rec in enumerate(rec_lst):
-                    key_nm = 'item ' + str(ind)
-                    dct[key_nm] = rec[1:-1]
-                    # change sorted as it doesnt work in our scenario
-                    # sorted(dct,dct.keys())
-                end_lst.append(dct)
-        return end_lst
 
