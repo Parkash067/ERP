dc2540376206572a2d2017a5b8e9a5abb2d62f6c
parkash067 <pk_bscs@yahoo.com>
2018-02-26 12:38:09 +0500

    add free minutes field + implementation of calculation of amount on the basis free minutes

--- custom_contracts/config.py
+++ custom_contracts/config.py
@@ -1,2 +1,2 @@
 import glob
-file_location = glob.glob('/usr/lib/python2.7/dist-packages/openerp/ERP/cdr_files/*.txt')
+file_location = glob.glob('/usr/lib/python2.7/dist-packages/openerp/custom_addons/cdr_files/*.txt')
